3ea12d0a3570ed672eb8cd5c1a2c2109d6af1e8b
Ghvstcode <impoissants@gmail.com>
2021-04-02 12:49:52 +0100

    Init Commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+require('dotenv').config()
+// const request = require("request");
+// const bodyParser = require("body-parser");
+
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", async (req, res) => {
+ res.send("Hello World").status(200)
+});
+
+app.listen(process.env.PORT, () => console.log(`App is running on port ${process.env.PORT||3000}`));
